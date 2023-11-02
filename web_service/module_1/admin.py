121280f8161c9a6c89c7a14f16667a09f5a46864
Nikermilo <nikermilo@gmail.com>
2023-11-02 19:18:43 +0300

    1st commit

---
+++ web_service/module_1/admin.py
@@ -0,0 +1,72 @@
+from django.contrib import admin
+
+from .models import Parent, Child, Administrator, Zav_UVR, Statement, Agreement
+
+
+class ParentAdmin (admin.ModelAdmin):
+    list_display = ('id', 'Parent_Code', 'Parent_FIO', 'Parent_Date', 'Parent_Home_Propiska', 'Parent_Home', 'Parent_SNILS', 'Parent_Phonenumeber', 'Parent_Passport')
+
+
+class ChildAdmin (admin.ModelAdmin):
+    def my_Child_Parent_Code(self, obj):
+        return obj.Child_Parent_Code.Parent_Code
+    
+    my_Child_Parent_Code.short_description = 'Код родителя'
+
+    list_display = ('id', 'Child_Code', 'Child_FIO', 'Child_Date', 'Child_Home_Propiska', 'Child_Home', 'Child_Gender', 'my_Child_Parent_Code')
+
+    search_fields = ('Child_FIO', 'Child_Parent_Code__Parent_Code')
+
+    raw_id_fields = ('Child_Parent_Code', )
+
+class AdministratorAdmin (admin.ModelAdmin):
+    list_display = ('id', 'Administrator_Tabel', 'Administrator_FIO', 'Administrator_Phonenumber', 'Administrator_Passport', 'Administrator_email', 'Administrator_Polis')
+    search_fields = ('Administrator_FIO', 'Administrator_Tabel', 'Administrator_Phonenumber')
+
+class Zav_UVRAdmin (admin.ModelAdmin):
+    list_display = ('id', 'Zav_UVR_Tabel', 'Zav_UVR_FIO', 'Zav_UVR_Phonenumber', 'Zav_UVR_Passport', 'Zav_UVR_email', 'Zav_UVR_Polis')
+    search_fields = ('Zav_UVR_FIO', 'Zav_UVR_Tabel', 'Zav_UVR_Phonenumber')
+
+class StatementAdmin (admin.ModelAdmin):
+    def my_Statement_Parent_Code(self, obj):
+        return obj.Statement_Parent_Code.Parent_Code
+    
+    def my_Statement_Administrator_Tabel(self, obj):
+        return obj.Statement_Administrator_Tabel.Administrator_Tabel
+    
+    my_Statement_Parent_Code.short_description = 'Код родителя'
+    my_Statement_Administrator_Tabel.short_description = 'Табельный номер администратора'
+    
+    list_display = ('id', 'Statement_Code', 'my_Statement_Parent_Code', 'my_Statement_Administrator_Tabel', 'Statement_Course', 'Statement_Group')
+
+    search_fields = ('Statement_Code', 'Statement_Administrator_Tabel__Administrator_Tabel', 'Statement_Parent_Code__Parent_Code')
+
+    """raw_id_fields = ('Statement_Parent_Code', )"""
+    
+class AgreementAdmin (admin.ModelAdmin):
+    def my_Agreement_Parent_Code(self, obj):
+        return obj.Agreement_Parent_Code.Parent_Code
+    def my_Agreement_Zav_UVR_Tabel(self, obj):
+        return obj.Agreement_Zav_UVR_Tabel.Zav_UVR_Tabel
+    def my_Agreement_Statement_Code(self, obj):
+        return obj.Agreement_Statement_Code.Statement_Code
+    def my_Agreement_Administrator_Tabel(self, obj):
+        return obj.Agreement_Administrator_Tabel.Administrator_Tabel
+    
+    my_Agreement_Parent_Code.short_description = 'Код родителя'
+    my_Agreement_Zav_UVR_Tabel.short_description = 'Табельный номер заведующего УВР'
+    my_Agreement_Statement_Code.short_description = 'Номер завления'
+    my_Agreement_Administrator_Tabel.short_description = 'Табельный номер администратора'
+
+    list_display = ('id', 'Agreement_Code', 'my_Agreement_Parent_Code', 'my_Agreement_Zav_UVR_Tabel', 'my_Agreement_Statement_Code', 'my_Agreement_Administrator_Tabel', 'Agreement_Course', 'Agreement_Group', 'Agreement_Requisites', 'Agreement_Price')
+
+    search_fields = ('Agreement_Code', 'Agreement_Parent_Code__Parent_Code', 'Agreement_Zav_UVR_Tabel__Zav_UVR_Tabel', 'Agreement_Statement_Code__Statement_Code', 'Agreement_Administrator_Tabel__Administrator_Tabel')
+
+    """raw_id_fields = ('Agreement_Course', 'Agreement_Group')"""
+
+admin.site.register(Parent, ParentAdmin)
+admin.site.register(Child, ChildAdmin)
+admin.site.register(Administrator, AdministratorAdmin)
+admin.site.register(Zav_UVR, Zav_UVRAdmin)
+admin.site.register(Statement, StatementAdmin)
+admin.site.register(Agreement, AgreementAdmin)
---
+++ web_service/module_1/models.py
@@ -0,0 +1,129 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+def gender_check(Child_Gender):
+    if Child_Gender not in ["Мужской", "Женский"]:
+        raise ValidationError(
+            gettext_lazy('%(Child_Gender)s is wrong gender'),
+            params={'Child_Gender': Child_Gender},
+        )
+
+class Parent(models.Model):
+    """"Родители"""
+
+    class Meta:
+        db_table = "parents"
+        verbose_name = "Родитель"
+        verbose_name_plural = "Родители"
+
+    Parent_Code = models.TextField(verbose_name="Код родителя")
+    Parent_FIO = models.TextField(verbose_name="ФИО Родителя")
+    Parent_Date = models.DateField(verbose_name="Дата рождения родителя")
+    Parent_Home_Propiska = models.TextField(verbose_name="Адрес прописки родителя")
+    Parent_Home = models.TextField(verbose_name="Место жительства родителя")
+    Parent_SNILS = models.TextField(verbose_name="СНИЛС родителя")
+    Parent_Phonenumeber = models.TextField(verbose_name="Номер телефона родителя")
+    Parent_Passport = models.TextField(verbose_name="Данные паспорта родителя")
+
+    def __str__(self):
+        return f"{self.Parent_FIO} {self.Parent_Date} {self.Parent_Home_Propiska} {self.Parent_Home} {self.Parent_SNILS} {self.Parent_Phonenumeber} {self.Parent_Passport} {self.Parent_Code}"
+
+class Child(models.Model):
+    """"Дети"""
+
+    class Meta:
+        db_table = "children"
+        verbose_name = "Ребёнок"
+        verbose_name_plural = "Дети"
+
+    Child_Code = models.TextField(verbose_name="Код обучающегося")
+    Child_FIO = models.TextField(verbose_name="ФИО ребёнка")
+    Child_Date = models.DateField(verbose_name="Дата рождения ребёнка")
+    Child_Home_Propiska = models.TextField(verbose_name="Адрес прописки ребёнка")
+    Child_Home = models.TextField(verbose_name="Место жительства ребёнка")
+    Child_Gender = models.TextField(verbose_name="Пол ребёнка", validators=[gender_check])
+    Child_Parent_Code = models.ForeignKey(Parent, on_delete=models.RESTRICT, verbose_name="ID родителя")
+
+    def __str__(self):
+        return f"{self.Child_Code} {self.Child_Date} {self.Child_FIO} {self.Child_Gender} {self.Child_Home} {self.Child_Home_Propiska} {self.Child_Parent_Code}"
+
+class Administrator(models.Model):
+    """"Администраторы"""
+
+    class Meta:
+        db_table = "Administrators"
+        verbose_name = "Администратор"
+        verbose_name_plural = "Администраторы"
+
+    Administrator_Tabel = models.TextField(verbose_name="Табельный номер администратора")
+    Administrator_FIO = models.TextField(verbose_name="ФИО администратора")
+    Administrator_Phonenumber = models.TextField(verbose_name="Номер телефона администратора")
+    Administrator_Passport = models.TextField(verbose_name="Данные паспорта администратора")
+    Administrator_email = models.TextField(verbose_name="Электронная почта администратора")
+    Administrator_Polis = models.TextField(verbose_name="Полис обязательного медицинского страхования администратора")
+
+
+    def __str__(self):
+        return f"{self.Administrator_FIO} {self.Administrator_Phonenumber} {self.Administrator_Passport} {self.Administrator_email} {self.Administrator_Polis} {self.Administrator_Tabel}"
+
+class Zav_UVR(models.Model):
+    """"Заведующий учебно-воспитательной работы"""
+
+    class Meta:
+        db_table = "Zav_UVR"
+        verbose_name = "Заведующий учебно-воспитательной работы"
+        verbose_name_plural = "Заведующие учебно-воспитательной работы"
+
+    Zav_UVR_Tabel = models.TextField(verbose_name="Табельный номер заведующего учебно-воспитательной работы")
+    Zav_UVR_FIO = models.TextField(verbose_name="ФИО заведующего учебно-воспитательной работы")
+    Zav_UVR_Phonenumber = models.TextField(verbose_name="Номер телефона заведующего учебно-воспитательной работы")
+    Zav_UVR_Passport = models.TextField(verbose_name="Данные паспорта заведующего учебно-воспитательной работы")
+    Zav_UVR_email = models.TextField(verbose_name="Электронная почта заведующего учебно-воспитательной работы")
+    Zav_UVR_Polis = models.TextField(verbose_name="Полис обязательного медицинского страхования заведующего учебно-воспитательной работы")
+
+
+    def __str__(self):
+        return f"{self.Zav_UVR_FIO} {self.Zav_UVR_Phonenumber} {self.Zav_UVR_Passport} {self.Zav_UVR_email} {self.Zav_UVR_Polis} {self.Zav_UVR_Tabel}"
+    
+class Statement(models.Model):
+    """"Заявление"""
+
+    class Meta:
+        db_table = "Statements"
+        verbose_name = "Заявление"
+        verbose_name_plural = "Заявления"
+
+    Statement_Code = models.TextField(verbose_name="Код заявления")
+    Statement_Parent_Code = models.ForeignKey(Parent, on_delete=models.RESTRICT, verbose_name="Родитель")
+    Statement_Administrator_Tabel = models.ForeignKey(Administrator, on_delete=models.RESTRICT, verbose_name="Администратор, готовящий заявление")
+    Statement_Course = models.TextField(verbose_name="Выбранный обучающий курс")
+    Statement_Group = models.TextField(verbose_name="Выбранная учебная группа")
+
+    def __str__(self):
+        return f"{self.Statement_Code} {self.Statement_Parent_Code} {self.Statement_Administrator_Tabel} {self.Statement_Course} {self.Statement_Group}"
+
+class Agreement(models.Model):
+    """"Итоговый договор"""
+
+    class Meta:
+        db_table = "Agreements"
+        verbose_name = "Итоговый договор"
+        verbose_name_plural = "Итоговые договора"
+
+    Agreement_Code = models.TextField(verbose_name="Код договора")
+    Agreement_Parent_Code = models.ForeignKey(Parent, on_delete=models.RESTRICT, verbose_name="Родитель")
+    Agreement_Zav_UVR_Tabel = models.ForeignKey(Zav_UVR, on_delete=models.RESTRICT, verbose_name="Заведующий учебно-воспитательной работой, готовящий договор")
+    Agreement_Statement_Code = models.ForeignKey(Statement, on_delete=models.RESTRICT, verbose_name="Заявление, являющееся основанием заключения договора")
+    Agreement_Administrator_Tabel = models.ForeignKey(Administrator, on_delete=models.RESTRICT, verbose_name="Администратор, готовящий заявление, являющееся основанием заключения договора")
+    Agreement_Course = models.TextField(verbose_name="Выбранный обучающий курс")
+    Agreement_Group = models.TextField(verbose_name="Выбранная учебная группа")
+    Agreement_Requisites = models.TextField(verbose_name="Реквизиты банка от компании")
+    Agreement_Price = models.IntegerField(verbose_name="Цена предоставления услуг")
+
+    def __str__(self):
+        return f"{self.Agreement_Code} {self.Agreement_Parent_Code} {self.Agreement_Zav_UVR_Tabel} {self.Agreement_Statement_Code} {self.Agreement_Administrator_Tabel} {self.Agreement_Course} {self.Agreement_Group} {self.Agreement_Requisites} {self.Agreement_Price}"
+    
